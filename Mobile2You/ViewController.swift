fa909775ec752742ca10ba184a24ddc223749665
Rychillie <rychillie@hotmail.com>
2021-10-12 18:40:24 -0300

    Initial Commit

---
+++ Mobile2You/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile2You
+//
+//  Created by Rychillie Umpierre de Oliveira on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
